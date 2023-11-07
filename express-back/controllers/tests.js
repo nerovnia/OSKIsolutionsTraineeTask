cbb9163896f537c4c888c8aa90af9e00559ccb2d
Volodymyr Nerovnia <nerv@i.ua>
2023-11-07 16:26:33 +0200

    Corrected tests controller

--- express-back/controllers/tests.js
+++ express-back/controllers/tests.js
@@ -1,9 +1,9 @@
 const getAllowedTests = async (req, res, next) => {
   try {
-    res.status(200).json(new ContactsReqDTO(result.email, result.address));
+    res.status(200).json({});
   } catch (err) {
     res.status(500).json({ message: "Помилка на боці серверу" });
   }
 };
 
-module.exports = { getContacts, updateContacts };
+module.exports = { getAllowedTests };
