0fadadb734272897a17d33f6ce7c2d673f6cc6ef
Volodymyr Nerovnia <nerv@i.ua>
2023-11-09 17:50:14 +0200

    Apdated configuration, changed token alive time

--- express-back/config/app.js
+++ express-back/config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
-  accessTokenAliveTime: 15 * 60 * 1000,
-  refreshTokenAliveTime: 3 * 24 * 60 * 60 * 1000,
+  accessTokenAliveTime: "15m",
+  refreshTokenAliveTime: "3d",
   MongoDB: {
     selectionTimeout: 30 * 1000,
     restartSec: 5 * 1000,
