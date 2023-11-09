774a3ad5ce6318bbef0d936aed2fa531f977ff2d
Volodymyr Nerovnia <nerv@i.ua>
2023-11-09 17:52:35 +0200

    Created data module with demo user accounts

---
+++ express-back/data/demo/users.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    login: "simon@gmail.com",
+    password: "nomis",
+    refreshToken: "",
+    allowedTests: ["landmarks"],
+    completedTests: [],
+  },
+  {
+    login: "lokky@gmail.com",
+    password: "ykkol",
+    refreshToken: "",
+    allowedTests: ["music"],
+    completedTests: [],
+  },
+  {
+    login: "jimmy@gmail.com",
+    password: "ymmij",
+    refreshToken: "",
+    allowedTests: ["landmarks", "music"],
+    completedTests: [],
+  },
+  {
+    login: "lora@gmail.com",
+    password: "arol",
+    refreshToken: "",
+    allowedTests: ["music"],
+    completedTests: [],
+  },
+];
