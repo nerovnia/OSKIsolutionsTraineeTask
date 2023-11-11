e2b2c00ef8257c2d822ca416695ba2925cf0ba28
Volodymyr Nerovnia <nerv@i.ua>
2023-11-11 02:46:38 +0200

    Enhanced configuration

--- express-back/config/app.js
+++ express-back/config/app.js
@@ -1,8 +1,16 @@
 module.exports = {
-  accessTokenAliveTime: "15m",
-  refreshTokenAliveTime: "3d",
+  accessTokenAliveTimeTxt: "15m",
+  refreshTokenAliveTimeTxt: "3d",
+  accessTokenAliveTime: 15 * 60 * 1000,
+  refreshTokenAliveTime: 3 * 24 * 60 * 60 * 1000,
   MongoDB: {
     selectionTimeout: 30 * 1000,
     restartSec: 5 * 1000,
+  },
+  authentication: {
+    password: {
+      min: 4,
+      max: 15,
+    },
   },
 };
